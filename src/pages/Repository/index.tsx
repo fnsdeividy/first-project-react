036b7bc4d3afc530968196b605fb67278ce49437
deividy <deividyferreira.n@gmail.com>
2021-09-25 05:39:12 -0300

    add style info

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom'
-import { Header } from './styles'
+import { useRouteMatch, Link } from 'react-router-dom'
+import { Header, RepositoryInfo, Issues } from './styles';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
 
 interface RepositoryParams {
     repository: string;
@@ -10,9 +11,50 @@ const Repository:React.FC = () => {
     const { params } = useRouteMatch<RepositoryParams>();
 
 return (
+    <>
     <Header>
-        <img src="https://xesque.rocketseat.dev/platform/1587379765556-attachment.svg" alt="Github Explorer" alt="" />
+        <img src="https://xesque.rocketseat.dev/platform/1587379765556-attachment.svg" alt="Github Explorer" />
+        <Link to="/">
+        <FiChevronLeft size={16} />
+        Voltar
+        </Link>
     </Header>
+
+    <RepositoryInfo>
+        <header>
+            <img src="https://avatars.githubusercontent.com/u/89440440?v=4" alt="Deividy Ferreira" />
+            <div>
+                <strong>Deividy nfsdeividy</strong>
+                <p>descrição do repositório</p>
+            </div>
+        </header>
+        <ul>
+            <li>
+                <strong>1888</strong>
+                <span>Stars</span>
+            </li>
+            <li>
+                <strong>48</strong>
+                <span>Forks</span>
+            </li>
+            <li>
+                <strong>67</strong>
+                <span>Issues Abertas</span>
+            </li>
+        </ul>
+    </RepositoryInfo>
+
+    <Issues>
+        <Link  to="lm,fdlam,flçã">
+
+            <div>
+                <strong>nlknlkds</strong>
+                <p>dfanjadslk f</p>
+            </div>
+        <FiChevronRight size={20}/>
+        </Link>
+    </Issues>
+    </>
 )
 };
 
